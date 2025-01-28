506abc8584b005cebd16b64b396643a5775bdc41
NastyaKitt <Vbloger23@gmail.com>
2025-01-28 11:01:15 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
-let popupBtn = document.getElementById('popup')
+let politicBtn = document.getElementById('popup')
+let clsPolitic = document.getElementById('cls')
 
-popupBtn.addEventListener("click", function() {
-    document.getElementById("my-modal").classList.add("open");
+politicBtn.addEventListener("click", function() {
+    document.getElementById("modalPolitic").classList.add("open");
+});
+clsPolitic.addEventListener("click", function() {
+    document.getElementById("modalPolitic").classList.remove("open");
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const reviews = document.querySelectorAll('.review');
+    const leftBtn = document.querySelector('.leftBtn');
+    const rightBtn = document.querySelector('.rightBtn');
+    let currentIndex = 0;
+
+    function updateReviews() {
+        reviews.forEach((review, index) => {
+            // Удаляем класс 'active' у всех
+            review.classList.remove('active');
+
+            // Если индекс совпадает с текущим, добавляем класс 'active'
+            if (index === currentIndex) {
+                review.classList.add('active');
+            }
+        });
+    }
+
+    rightBtn.addEventListener('click', function() {
+        currentIndex = (currentIndex + 1) % reviews.length; // Переход к следующему отзыву
+        updateReviews(); // Обновляем отображение отзывов
+    });
+
+    leftBtn.addEventListener('click', function() {
+        currentIndex = (currentIndex - 1 + reviews.length) % reviews.length; // Переход к предыдущему отзыву
+        updateReviews(); // Обновляем отображение отзывов
+    });
+
+    // Инициализируем отображение с первым отзывом
+    updateReviews();
 });
